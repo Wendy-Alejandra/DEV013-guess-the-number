4909550a737f0a7ef519cfc85030770979c25821
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-04-03 17:02:16 -0500

    Test file added and comments added to the file

---
+++ main_test.py
@@ -0,0 +1,4 @@
+import unittest # Unit test framework (standard library) integrated in Python
+from unittest.mock import patch # patch is used to replace parts of the code being tested with mock objects
+from io import StringIO # Evaluates inputs/outputs from print and input functions
+import sys
