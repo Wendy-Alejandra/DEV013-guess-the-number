81809017102878a97cc90d532eac1b70a89e780a
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-04-04 17:18:30 -0500

    Fix computer_guess() call within game_rounds() function so it doesn´t change its guess when the function is called

--- main.py
+++ main.py
@@ -32,33 +32,42 @@ def computer_guess():
 # Game rounds
 def game_rounds():
     """Logic game, gives the option to continue or finish the game once someone wins"""
-    continue_game = "y"
-    while continue_game == "y":
-        player = []
-        computer = []
-        generated_number = generate_random_number()
-        print(generated_number)
-        player_name = menu_game()
-        rounds = 0
-        while True:     # Executes the game till either the player or the computer wins the game
-            rounds+=1
-            print(f"***************** Round {rounds}*****************\n")
-            try:
-                player_guess = int(input(f"{player_name}, please enter a number between 1-100: "))
-            except ValueError:
-                print("You should enter numbers only, try again\n")
-            if guess_validation(player_guess, generated_number):
-                player.append(player_guess)
-                print(f"\nCongrats {player_name}! {generated_number} was the correct number!!")
-                print(f"You had {rounds} attemps: {', '.join(map(str, player))} ")
-                break # Ends the game when the player wins
+    continue_game = ""
+    # while continue_game == "y":
+    player = []
+    computer = []
+    generated_number = generate_random_number()
+    print(generated_number)
+    player_name = menu_game()
+    rounds = 0
+    while True:     # Executes the game till either the player or the computer wins the game
+        rounds+=1
+        print(f"***************** Round {rounds}*****************\n")
+        try:
+            player_guess = int(input(f"{player_name}, please enter a number between 1-100: "))
+        except ValueError:
+            print("You should enter numbers only, try again\n")
+        if guess_validation(player_guess, generated_number):
             player.append(player_guess)
-            print(f"Computer's guess: {computer_guess()}")
-            if guess_validation(computer_guess(), generated_number):
-                print(f"Congrats computer! {generated_number} was the correct number!! ")
-                print(f"You had {rounds} attemps as follows: {', '.join(map(str, computer))}")
-                break # Ends the game when the computer wins
-            computer.append(computer_guess())
-        continue_game = input("\nWould you like to continue the game? y/n ")
+            print(f"\nCongrats {player_name}! {generated_number} was the correct number!!")
+            print(f"You had {rounds} attemps: {', '.join(map(str, player))} ")
+            continue_game = input("\nWould you like to continue the game? y/n ")
+            break # Ends the game when the player wins
+        else:
+            player.append(player_guess)
+        # Store computer_guess() in a variable comp_guess inside the loop so it is not regenerated every time the function is called and it's not static if stored ouside
+        # the loop
+        comp_guess = computer_guess()
+        print(f"Computer's guess: {comp_guess}")
+        if guess_validation(comp_guess, generated_number):
+            computer.append(comp_guess)
+            print(f"Congrats computer! {generated_number} was the correct number!! ")
+            print(f"You had {rounds} attemps as follows: {', '.join(map(str, computer))}")
+            continue_game = input("\nWould you like to continue the game? y/n ")
+            # if continue_game == "y" or continue_game == "Y":
+            break # Ends the game when the computer wins
+        else:
+            computer.append(comp_guess)
+        
 if __name__ == '__main__':
     game_rounds()
